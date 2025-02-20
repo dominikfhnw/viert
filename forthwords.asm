bd512f758543d82687b7e2c869e279f564a06288
Dominik Russenberger <dominik.russenberger@students.fhnw.ch>
2025-02-20 11:09:42 +0100

    words: dec, div, mod, emit, nl. first version of dot

--- forthwords.asm
+++ forthwords.asm
@@ -3,6 +3,25 @@ DEFFORTH "exit"
 	f_syscall3
 ENDDEF noreturn
 
+%if 0
+DEFFORTH "dec"
+	lit 1
+	f_negate
+	f_plus
+ENDDEF
+%endif
+
+DEFFORTH "div"
+	f_divmod
+	f_drop
+ENDDEF
+
+DEFFORTH "mod"
+	f_divmod
+	f_swap
+	f_drop
+ENDDEF
+
 DEFFORTH "puts"
 	f_swap
 	lit	1 ;stdout
@@ -11,15 +30,59 @@ DEFFORTH "puts"
 	f_drop
 ENDDEF
 
-DEFFORTH "heya"
-	string "heya"
+DEFFORTH "emit"
+	f_sp_at
+	lit 1
 	f_puts
+	f_drop
 ENDDEF
 
-DEFFORTH "triple"
-	f_heya
-	f_heya
-	f_heya
+DEFFORTH "nl"
+	lit `\n`
+	f_emit
 ENDDEF
 
+DEFFORTH "char"
+	lit 'E'
+	f_emit
+ENDDEF
 
+DEFFORTH "dotstep"
+	; divide TOS by 10
+	lit 10
+	f_divmod
+
+	; convert to ascii
+	lit '0'
+	f_plus
+
+	; print
+	f_emit
+
+ENDDEF
+
+DEFFORTH "dot"
+	;string `parsing...\n`
+	;f_puts
+	%if 0
+		lit 8
+		.loop:
+		f_dotstep
+		f_dec
+		f_zbranch
+		db .loop - $ - 1
+	%else
+		f_dotstep
+		f_dotstep
+		f_dotstep
+		f_dotstep
+		f_dotstep
+		f_dotstep
+		f_dotstep
+		f_dotstep
+		f_dotstep
+	%endif
+
+	f_nl
+
+ENDDEF
