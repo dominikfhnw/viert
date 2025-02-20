627fb6092eefeac97938f0661455f4a024cd6cf5
Dominik Russenberger <dominik.russenberger@students.fhnw.ch>
2025-02-20 11:10:38 +0100

    factor out code, less warning, make DEFn just aliases (easier debugging)

--- macros.asm
+++ macros.asm
@@ -26,18 +26,15 @@
 			%error not working atm
 			;lea	NEXT_WORD, [WORD_ALIGN*NEXT_WORD+ASM_OFFSET]
 		%endif
-		jmp	NEXT_WORD
 
 	%elif !WORD_TABLE && WORD_SIZE == 1
 		%if WORD_ALIGN > 1
 			xor	eax, eax
 			lodsWORD
 			lea	eax, [eax*WORD_ALIGN+TABLE_OFFSET]
-			jmp	NEXT_WORD
 		%else
 			mov	eax, TABLE_OFFSET
 			lodsWORD
-			jmp	NEXT_WORD
 		%endif
 	%elif WORD_SMALLTABLE
 		set	NEXT_WORD, 0
@@ -46,11 +43,10 @@
 		mov	eax, [STATIC_TABLE + 2*NEXT_WORD]
 		cwde
 		add	eax, TABLE_OFFSET
-		jmp	NEXT_WORD
-
 	%else
 		%error unhandled case
 	%endif
+	jmp	NEXT_WORD
 %endmacro
 
 %macro DEF 1-2.nolist
@@ -60,7 +56,8 @@
 	align WORD_ALIGN, nop
 	; objdump prints the lexicographical smallest label. change A to E
 	; or something to get the DEFn labels
-	DEF%[WORD_COUNT]:
+	;DEF%[WORD_COUNT]:
+	%define DEF%[WORD_COUNT] A_%tok(%1)
 	A_%tok(%1):
 	%define %[n_%tok(%1)] %[WORD_COUNT]
 	%define %[f_%tok(%1)] WORD %[WORD_COUNT]
@@ -135,12 +132,12 @@
 
 %imacro lit 1
 	%if %1 >= 0 && %1 < 256
-		%warning FOO f_lit8 -- %1
+		;%warning FOO f_lit8 -- %1
 		f_lit8
-		%warning FOO2 f_lit8 -- %1
+		;%warning FOO2 f_lit8 -- %1
 		db %1
 	%else
-		%warning FOO3 f_lit8 -- %1
+		;%warning FOO3 f_lit8 -- %1
 		f_lit32
 		dd %1
 	%endif
