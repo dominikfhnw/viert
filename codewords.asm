0dc80c634832e0c0c0e4deb3c67f024e0872065a
Dominik Russenberger <dominik.russenberger@students.fhnw.ch>
2025-02-21 23:52:02 +0100

    decide primitive/forth word by opcode

--- codewords.asm
+++ codewords.asm
@@ -9,12 +9,14 @@ DEF "lit8"
 	lodsb
 	push	eax
 
+%if 0
 DEF "lit16"
 	lodsb
 	mov	ah, al
 	lodsb
 	cwde
 	push	eax
+%endif
 
 DEF "lit32"
 	lodsd
@@ -102,7 +104,103 @@ DEF "zbranch"
 	branch:
 	;inc	esi
 	
-DEF "string"
+
+; **** INIT BLOCK ****
+NEXT
+;_start:
+;%include "init.asm"
+
+%if !THRESH
+	DEF "DOCOL", no_next
+		rspush	FORTH_OFFSET
+		inc	eax
+		inc	eax
+		xchg	eax, esi
+%endif
+
+
+; TODO: merge with WORDVAL macro
+%if WORD_TABLE
+	%define BREAK 17
+%else
+	%define BREAK (END_OF_CODEWORDS - ASM_OFFSET + ELF_HEADER_SIZE)/WORD_ALIGN
+%endif
+
+
+A_DOCOL:
+rspush	FORTH_OFFSET
+xchg	eax, esi
+
+
+A_NEXT:
+
+%if !WORD_TABLE && WORD_SIZE == 2
+	mov	eax, TABLE_OFFSET
+	lodsWORD
+	cmp	ax, BREAK
+	jae	A_DOCOL
+	jmp	eax
+	%if WORD_ALIGN > 1
+		%error not working atm
+		;lea	NEXT_WORD, [WORD_ALIGN*NEXT_WORD+ASM_OFFSET]
+	%endif
+
+%elif !WORD_TABLE && WORD_SIZE == 1
+	%if THRESH
+		%if WORD_ALIGN > 1
+			xor	eax, eax
+			lodsWORD
+			cmp	al, BREAK
+			lea	eax, [eax*WORD_ALIGN+TABLE_OFFSET]
+		%else
+			mov	eax, TABLE_OFFSET
+			lodsWORD
+			cmp	al, BREAK
+		%endif
+		jae	A_DOCOL
+		jmp	eax
+	%else
+		%if WORD_ALIGN > 1
+			xor	eax, eax
+			lodsWORD
+			lea	eax, [eax*WORD_ALIGN+TABLE_OFFSET]
+		%else
+			mov	eax, TABLE_OFFSET
+			lodsWORD
+		%endif
+		jmp	eax
+	%endif
+%elif WORD_SMALLTABLE
+	%if THRESH
+		taint	eax
+		set	NEXT_WORD, 0
+		lodsWORD
+		cmp	al, BREAK
+
+		mov	eax, [STATIC_TABLE + 2*NEXT_WORD]
+		cwde
+		;add	eax, TABLE_OFFSET
+		lea	eax, [eax + TABLE_OFFSET]
+		jae	A_DOCOL
+		jmp	eax
+
+	%else
+		set     NEXT_WORD, 0
+		lodsWORD
+		mov     eax, [STATIC_TABLE + 2*NEXT_WORD]
+		cwde
+		add     eax, TABLE_OFFSET
+		jmp	eax
+
+	%endif
+%else
+	%error unhandled case
+%endif
+
+; **** END INIT ****
+
+
+DEF "string", no_next
 	xor	eax,eax
 	lodsb
 	push	esi
@@ -137,89 +235,8 @@ DEF "syscall3", no_next
 	push	eax
 
 NEXT
-_start:
-%include "init.asm"
-
-%if !THRESH
-	DEF "DOCOL", no_next
-		rspush	FORTH_OFFSET
-		inc	eax
-		inc	eax
-		xchg	eax, esi
-%endif
-
-A_NEXT:
-
-; TODO: merge with WORDVAL macro
-%if WORD_TABLE
-	%define BREAK WORD_COUNT
-%else
-	%define BREAK ($ - ASM_OFFSET + ELF_HEADER_SIZE)/WORD_ALIGN
-%endif
-
-%if !WORD_TABLE && WORD_SIZE == 2
-	mov	eax, TABLE_OFFSET
-	lodsWORD
-	cmp	al, BREAK
-	jae	.docol
-	jmp	eax
-	%if WORD_ALIGN > 1
-		%error not working atm
-		;lea	NEXT_WORD, [WORD_ALIGN*NEXT_WORD+ASM_OFFSET]
+END_OF_CODEWORDS:
+	%warning "BREAK" WORD_COUNT
+	%if WORD_TABLE == 1 && WORD_COUNT != BREAK
+		%error break constant set to wrong value: WORD_COUNT != BREAK
 	%endif
-
-%elif !WORD_TABLE && WORD_SIZE == 1
-	%if THRESH
-		%if WORD_ALIGN > 1
-			xor	eax, eax
-			lodsWORD
-			cmp	al, BREAK
-			lea	eax, [eax*WORD_ALIGN+TABLE_OFFSET]
-		%else
-			mov	eax, TABLE_OFFSET
-			lodsWORD
-			cmp	al, BREAK
-		%endif
-		jae	.docol
-		jmp	eax
-	%else
-		%if WORD_ALIGN > 1
-			xor	eax, eax
-			lodsWORD
-			lea	eax, [eax*WORD_ALIGN+TABLE_OFFSET]
-		%else
-			mov	eax, TABLE_OFFSET
-			lodsWORD
-		%endif
-		jmp	eax
-	%endif
-%elif WORD_SMALLTABLE
-	%if THRESH
-		set	NEXT_WORD, 0
-		lodsWORD
-		cmp	al, BREAK
-
-		mov	eax, [STATIC_TABLE + 2*NEXT_WORD]
-		cwde
-		;add	eax, TABLE_OFFSET
-		lea	eax, [eax + TABLE_OFFSET]
-		jae	.docol
-		jmp	eax
-
-	%else
-		set     NEXT_WORD, 0
-		lodsWORD
-		mov     eax, [STATIC_TABLE + 2*NEXT_WORD]
-		cwde
-		add     eax, TABLE_OFFSET
-		jmp	eax
-
-	%endif
-%else
-	%error unhandled case
-%endif
-
-.docol:
-	rspush	FORTH_OFFSET
-	xchg	eax, esi
-	jmp	A_NEXT
