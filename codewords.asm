6eda3f3dfed94c68b6b3a924aad21f6f2fcecc80
Dominik Russenberger <dominik.russenberger@students.fhnw.ch>
2025-03-01 15:44:25 +0100

    use END statement after DEFs; lots of experimental new codewords

--- codewords.asm
+++ codewords.asm
@@ -34,61 +34,101 @@
 ; TOTAL:	-3
 
 ; first definition does not need a NEXT
-DEF "EXIT", no_next
+DEF "EXIT"
 	rspop	FORTH_OFFSET
+	END
 
 DEF "sp_at"
 	push	esp
+	END
 
 DEF "swap"
+%if 0
 	pop	ecx
 	pop	edx
 	push	ecx
 	push	edx
+	END
+%else
+	pop	edx
+	pop	eax
+	jmp	pushedxeax
+	END no_next
+%endif
 
 DEF "dup"
+%if 1
 	push	dword [esp]
+	END
+%else
+	pop	eax
+	jmp	pusheax
+	END no_next
+%endif
 
 DEF "over"
 	push	dword [esp+4]
+	END
 
 DEF "drop"
 	pop	edx
+	END
 
 DEF "rot"
+%if 0
 	pop	edx
 	pop	ebx
 	pop	ecx
 	push	ebx
 	push	edx
 	push	ecx
+	END
+%else
+	pop	edx
+	pop	ecx
+	pop	eax
+	push	ecx
+	jmp	pushedxeax
+	END no_next
+%endif
 
 %define BIGJMP 0
+%if 0
+
 DEF "store"
 	pop	edx
 	pop	dword [edx] ; I have to agree with Kragen here, I'm also amazed this is legal
+	END
 
 DEF "fetch"
 	pop	edx
 	push	dword [edx] ; This feels less illegal for some reason
+	END
 
 DEF "cstore"
 	pop	ecx
 	pop	edx
 	mov	[ecx], dl
+	END
 
 DEF "cfetch"
 	pop	edx
 	mov	al, [edx]
+	END
+
+%endif
 
 %if 0
 DEF "dupr2d"
 	push	dword [RETURN_STACK]
-%endif
+	END
 
 DEF "rspop"
 	rspop	edx
 	push	edx
+	END
+
+%endif
 
 DEF "rspush"
 	%if 0
@@ -98,15 +138,17 @@ DEF "rspush"
 	lea	RETURN_STACK, [RETURN_STACK-4]
 	pop	dword [RETURN_STACK] ; would be one byte less if !ebp
 	%endif
+	END
 
 ;DEF "rsdec"
 ;	dec	dword [RETURN_STACK]
+;END
 
-DEF "rsinc"
-	inc	dword [RETURN_STACK]
+;DEF "rsinc"
+;	inc	dword [RETURN_STACK]
+;END
 
 ; **** INIT BLOCK ****
-NEXT
 
 A_DOCOL:
 rspush	FORTH_OFFSET
@@ -195,17 +237,110 @@ A_NEXT:
 
 ; **** END INIT ****
 
-DEF "zbranch", no_next
+DEF "zbranch"
 	pop	ecx
 	jecxz	A_branch
 	inc	FORTH_OFFSET
+	END
 
 DEF "branch"
 	movsx	edx, byte [FORTH_OFFSET]
 	add	FORTH_OFFSET, edx
+	END
 
-;DEF "branch32"
-;	add	FORTH_OFFSET, [FORTH_OFFSET]
+%if 0
+DEF "branchstack"
+	;movsx	edx, byte [FORTH_OFFSET]
+	pop	edx
+	add	FORTH_OFFSET, edx
+	END
+
+DEF "upget"
+	xchg	FORTH_OFFSET, ebx
+	lodsb
+	xchg	FORTH_OFFSET, ebx
+	END
+
+DEF "upbranch"
+	xchg	FORTH_OFFSET, ebx
+	lodsb
+	xchg	FORTH_OFFSET, ebx
+	add	FORTH_OFFSET, eax
+	END
+
+DEF "upget"
+	xchg	FORTH_OFFSET, [RETURN_STACK]
+	lodsb
+	xchg	FORTH_OFFSET, [RETURN_STACK]
+	;nop
+	;mov	edx, [RETURN_STACK]
+	;movzx	ecx, byte [edx+1]
+	;inc	dword [RETURN_STACK]
+	END
+
+DEF "while3"
+	lodsb			; load jump offset
+	dec	dword [RETURN_STACK]	; decrement loop counter
+
+	jz	A_rdrop		; clean up return stack if we're finished
+	;movsx	eax, al		; convert to -128..127 range
+	sub	FORTH_OFFSET, eax
+	END
+
+DEF "branchf"
+	lodsb
+	add	FORTH_OFFSET, eax
+	END
+
+DEF "branchb"
+	lodsb
+	sub	FORTH_OFFSET, eax
+	END
+
+DEF "zbranchf"
+	lodsb
+	pop	ecx
+	jecxz	A_NEXT
+	add	FORTH_OFFSET, eax
+	END
+
+DEF "zbranchb"
+	lodsb
+	pop	ecx
+	jecxz	A_NEXT
+	add	FORTH_OFFSET, eax
+	END
+
+DEF "zbranch2"
+	lodsb
+	pop	ecx
+	jecxz	A_NEXT
+	movsx	edx, al
+	add	FORTH_OFFSET, edx
+	END
+
+DEF "zbranch4"
+	;lodsb
+	pop	ecx
+	jecxz	A_NEXT
+	movsx	edx, byte [FORTH_OFFSET]
+	add	FORTH_OFFSET, edx
+	END
+
+
+DEF "upget8"
+	mov	eax, [ebp]
+	inc	dword [ebp]
+	movzx	eax, byte [eax]
+	push	eax
+	END
+
+DEF "upget32"
+	mov	eax, [ebp]
+	inc	dword [ebp]
+	push	dword [eax]
+	END
+%endif
 
 ; f_while <imm8>:
 ;  1. decrement an unspecified loop counter
@@ -218,16 +353,37 @@ DEF "while2"
 	jz	A_rdrop		; clean up return stack if we're finished
 	;movsx	eax, al		; convert to -128..127 range
 	sub	FORTH_OFFSET, eax
+	END
 
 DEF "rdrop"
 	lea	RETURN_STACK, [RETURN_STACK+4]
-
+	END
 
 DEF "string"
 	lodsb
 	push	FORTH_OFFSET
 	push	eax
 	add	FORTH_OFFSET, eax
+	END
+
+%if 1
+DEF "bool"
+	pop	ecx
+	jecxz	.zero
+	or	ecx, -1
+	.zero:
+	push	ecx
+	END
+
+DEF "bool2"
+        pop	ecx
+	test	ecx, ecx
+	setnz	al                ; AL=0 if ZF=1, else AL=1
+	dec	eax                  ; AL=ff if AL=0, else AL=0
+	;cbw                     ; AH=AL
+	push eax
+	END
+%endif
 
 DEF "plus"
 %if 0
@@ -239,21 +395,54 @@ DEF "plus"
 	pop	edx
 	add	[esp], edx
 %endif
+	END
 
-DEF "negate"
-	neg	dword [esp]
+;DEF "negate"
+;	neg	dword [esp]
 
+DEF "not"
+	not	dword [esp]
+	END
+
+;DEF "not2"
+;	pop	eax
+;	not	eax
+;	jmp	pusheax
+;	END no_next
+
+DEF "and"
+	pop	edx
+	and	[esp], edx
+	END
+
+DEF "or"
+	pop	edx
+	or	[esp], edx
+	END
+
+DEF "nand"
+	pop	edx
+	pop	eax
+	and	eax, edx
+	not	eax
+	jmp	pusheax
+	END no_next
+
+%if 0
 DEF "dec"
 	dec	dword [esp]
+	END
 
 DEF "inc"
 	inc	dword [esp]
 	;pop	edx
 	;inc	edx
 	;push	edx
+	END
+%endif
 
 DEF "divmod"
-	cdq
+	cdq		; eax is <= 255, so cdq will always work
 	pop	ecx
 	pop	eax
 	div	ecx
@@ -262,13 +451,18 @@ pushedxeax:
 pusheax:
 	push	eax
 	xor	eax, eax
+	END
 
+%if 0
 DEF "int3"
 	int3
+	END
+%endif
 
 DEF "lit8"
 	lodsb
 	jmp	pusheax
+	END no_next
 
 %if 0
 DEF "lit16"
@@ -276,15 +470,17 @@ DEF "lit16"
 	mov	ah, al
 	lodsb
 	cwde
-	push	eax
+	jmp	pusheax
+	END no_next
 %endif
 
-DEF "lit32", no_next
+DEF "lit32"
 	lodsd
 	jmp	pusheax
+	END no_next
 
 
-DEF "syscall3", no_next
+DEF "syscall3"
 %if 0
 	pop	edx
 	pop	ecx
@@ -299,13 +495,15 @@ DEF "syscall3", no_next
 
 	int	0x80
 	jmp	pusheax
+	END no_next
 
 
-%if 1
+%if 0
 DEF "asmjmp"
 	pop	edx
 	pop	edx
 	jmp	edx
+	END no_next
 %else
 
 ;DEF "asmret"
