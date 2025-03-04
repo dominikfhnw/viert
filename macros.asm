a31f6cd2bccc60bb34c332d3976dda37f24fe83d
Dominik Russenberger <dominik.russenberger@students.fhnw.ch>
2025-03-04 21:04:46 +0100

    cleanup macro code

--- macros.asm
+++ macros.asm
@@ -3,18 +3,10 @@
 %define JMPLEN short
 %endif
 
-%if OFFALIGN
-	%define offset(a)	(a - ASM_OFFSET + ELF_HEADER_SIZE)/WORD_ALIGN
-%else
-	%define offset(a)	(a - ASM_OFFSET)/WORD_ALIGN
-%endif
+%define offset(a)	(a - ASM_OFFSET)/WORD_ALIGN
 
 %macro NEXT arg(0)
-	%if BIGJMP
-		jmp    [BASE]
-	%else
-		jmp JMPLEN A_NEXT
-	%endif
+	jmp JMPLEN A_NEXT
 %endmacro
 
 %macro DEF 1-2.nolist
@@ -24,19 +16,13 @@
 	%push defcode
 
 	align WORD_ALIGN, nop
-	; objdump prints the lexicographical smallest label. change A to E
-	; or something to get the DEFn labels
-	;DEF%[WORD_COUNT]:
 	%define DEF%[WORD_COUNT] A_%tok(%1)
 	A_%tok(%1):
 	%define %[n_%tok(%1)] %[WORD_COUNT]
 	%define %[f_%tok(%1)] WORD %[WORD_COUNT]
 	%warning NEW DEFINITION: DEF%[WORD_COUNT] %1
-	;%define %[%tok(%1)] WORD %[WORD_COUNT]
 	rtaint
-	;%if !WORD_SMALLTABLE && WORD_TABLE
-		rset	NEXT_WORD, -2 ; 8-bit value
-	;%endif
+	rset	NEXT_WORD, -2 ; 8-bit value
 	%assign WORD_COUNT WORD_COUNT+1
 %endmacro
 
@@ -44,16 +30,11 @@
 	%ifctx defforth
 		%fatal Nested DEFFORTH not allowed. Did you forget an ENDDEF?
 	%endif
-	;%push defforth
-	;%$current:
 
 	; another align here, to override "align with nop"
 	align WORD_ALIGN, db 0
 	DEF %1, no_next
 	%repl defforth
-	%if !THRESH
-		DOCOL
-	%endif
 %endmacro
 
 %macro END 0-1
@@ -71,40 +52,10 @@
 %endmacro
 %define ENDDEF END
 
-%if WORD_TABLE
-	%define WORDVAL(a) a
-%elif WORD_SIZE == 4
-	%define WORDVAL(a) DEF%tok(a)
-%else
-	%define WORDVAL(a) offset(DEF%tok(a))
-%endif
+%define WORDVAL(a) offset(DEF%tok(a))
 
 %macro WORD 1
 	WORD_DEF WORDVAL(%1)
-%endmacro
-
-; XXX unused?
-;%macro OVERRIDE_NEXT 1
-;	push n_%[%1]
-;	set FORTH_OFFSET, esp
-;%endmacro
-
-;%macro DIRECT_EXECUTE 1
-;	jmp A_%[%1]
-;%endmacro
-
-; execute 2 words. Undefined behaviour if the second word doesn't exit
-; Second word has to be already defined.
-; Second word is expected to be something like "exit"
-; XXX unused?
-;%macro EXECUTE2 2
-;	OVERRIDE_NEXT	%2
-;	DIRECT_EXECUTE	%1
-;%endmacro
-
-%macro DOCOL arg(0)
-	jmp short A_DOCOL
-	%%forth:
 %endmacro
 
 %imacro rspop arg(1)
