68c9115a0c9809e26e27e866a9910c8f9744d559
Dominik Russenberger <dominik.russenberger@students.fhnw.ch>
2025-03-05 11:31:22 +0100

    further simplify init.asm

--- init.asm
+++ init.asm
@@ -1,18 +1,11 @@
 enter	0xFFFF, 0
-%if !WORD_TABLE && WORD_SIZE == 4
+%if OFFALIGN
+	%if BASEREG
+		set	BASE, ORG
+	%endif
 %else
-	%if WORD_TABLE ==  1 && WORD_SMALLTABLE == 0
-		mov	edi, STATIC_TABLE
-	%else
-		%if OFFALIGN
-			%if BASEREG
-				set	BASE, ORG
-			%endif
-		%else
-			%if BASEREG
-				mov	BASE, ASM_OFFSET
-			%endif
-		%endif
+	%if BASEREG
+		mov	BASE, ASM_OFFSET
 	%endif
 %endif
 
