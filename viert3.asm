53a14eee22ef1ef6cd40964e536f3e74e942bde7
Dominik Russenberger <dominik.russenberger@students.fhnw.ch>
2025-03-08 09:19:35 +0100

    get rid of init.asm

--- viert3.asm
+++ viert3.asm
@@ -195,15 +195,22 @@ SECTION .text align=1
 
 
 A_INIT:
-;_start:
 ; "enter" will push ebp on the stack
 ; This means that we can call EXIT to start the forth code at ebp
 ; And conveniently, EXIT is the first defined word, so we can "call" it
 ; by simply doing nothing
 mov	ebp, FORTH
-%include "init.asm"
+enter	0xFFFF, 0
+%if OFFALIGN
+	%if BASEREG
+		set	BASE, ORG
+	%endif
+%else
+	%if BASEREG
+		mov	BASE, ASM_OFFSET
+	%endif
+%endif
 ELF_PHDR 1
-align WORD_ALIGN
 
 ASM_OFFSET:
 %include "codewords.asm"
