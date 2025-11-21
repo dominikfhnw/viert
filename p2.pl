5c0044edacae487d6deda59ae7ebe4b5dc1b3b2b
Dominik Russenberger <dominik.russenberger@students.fhnw.ch>
2025-11-21 12:31:48 +0100

    mark rsdrop as an available asm function

--- p2.pl
+++ p2.pl
@@ -105,6 +105,7 @@ my %builtin_all = map { $_ => 1 } qw(
 	0< 1- EXIT bye divmod drop dup @ i < - nand not over + rp@ rpsp@ rsinci rspush sp@ !
 	swap 2*
 	stringr string dotstr string0
+	rsdrop
 	if else unless do swapdo loop loople begin again until notuntil next
 	syscall3_noret syscall3
 	syscall7
